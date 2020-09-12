71d1834fda383fc9717b149c65d2db6e5807e733
Jens Weller <info@meetingcpp.com>
2020-09-12 17:04:06 +0200

    adding rasterize prototype

---
+++ rasterizedshape.cpp
@@ -0,0 +1,9 @@
+#include "rasterizedshape.h"
+
+namespace cpp20tshirt
+{
+RasterizedShape::RasterizedShape(PixelGroup&& pg):group(std::move(pg))
+{
+
+}
+}
---
+++ rasterizedshape.h
@@ -0,0 +1,11 @@
+#ifndef RASTERIZEDSHAPE_H
+#define RASTERIZEDSHAPE_H
+
+
+class RasterizedShape
+{
+public:
+    RasterizedShape();
+};
+
+#endif // RASTERIZEDSHAPE_H
