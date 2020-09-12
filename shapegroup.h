a586af14679dd54830cb4565dceb859f12027bc3
Jens Weller <info@meetingcpp.com>
2020-09-12 17:25:34 +0200

    add shapegroup

---
+++ shapegroup.h
@@ -0,0 +1,25 @@
+#ifndef SHAPEGROUP_H
+#define SHAPEGROUP_H
+
+#include <vector>
+
+template<class Shape>
+class ShapeGroup
+{
+   std::vector<Shape> group;
+public:
+   void addShape(Shape&& s){group.emplace_back(std::move(s));}
+   void step(){
+       for(auto& shape:group)
+           shape.step();
+   }
+   template<class CB>
+   void visit(const CB& cb)const
+   {
+       for(const auto& ps:group)
+           cb(ps);
+   }
+   auto size()const{return group.size();}
+};
+
+#endif // SHAPEGROUP_H
